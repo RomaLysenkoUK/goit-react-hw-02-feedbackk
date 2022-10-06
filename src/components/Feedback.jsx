712e147e5661264000b39282d675bbaa6f71ffb0
Roman Lysenko <romashkaxxxl@gmail.com>
2022-10-06 21:01:42 +0300

    add task

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -2,27 +2,36 @@
 //import { Paragraf, Span } from './User.styled';
 //import { countGoodFeedback } from './App'
 
-export const Feedback = ({ options: { good, neutral, bad }, countGoodFeedback}) => {
-   
-    
-    return (
-        <>
-            <h2>Please leave feedback</h2>
-            <button name="good" type="button" onClick={countGoodFeedback}>Good</button>
-            <button name="neutral" type="button" onClick={countGoodFeedback}>Neutral</button>
-            <button name="bad" type="button" onClick={countGoodFeedback}>Bad</button>
-            <h2>Statistics</h2>
-            <ul>
-                <li>Good: {good}</li>
-                <li>Neutral: {neutral}</li>
-                <li>Bad: {bad}</li>
-                <li>Total: {bad}</li>
-                <li>Positive feedback: {bad}%</li>
-            </ul>
-
-        </>
-    );
-}
+export const Feedback = ({
+  options: { good, neutral, bad },
+  countFeedback,
+  countTotalFeedback,
+  countPositiveFeedbackPercentage,
+}) => {
+  console.log(countTotalFeedback());
+  return (
+    <>
+      <h2>Please leave feedback</h2>
+      <button name="good" type="button" onClick={countFeedback}>
+        Good
+      </button>
+      <button name="neutral" type="button" onClick={countFeedback}>
+        Neutral
+      </button>
+      <button name="bad" type="button" onClick={countFeedback}>
+        Bad
+      </button>
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {countTotalFeedback()}</li>
+        <li>Positive feedback: {countPositiveFeedbackPercentage()}%</li>
+      </ul>
+    </>
+  );
+};
 
 // User.propTypes = {
 //     user: PropTypes.shape({
